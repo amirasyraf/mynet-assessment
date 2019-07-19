593fd5161857798cbe61fa1a288ca74e296345e3
amirasyraf <amirasyraf@outlook.com>
2019-07-19 15:12:38 +0000

    updateMember() function

--- crud.php
+++ crud.php
@@ -19,6 +19,24 @@ function retrieveMember() {
 }
 
 function updateMember() {
+	global $connection;
+
+	try {
+
+	    $sql = "UPDATE member
+	    SET fullname = :fullname, mykad = :mykad, email = :email, date_registered = :date_registered
+	    WHERE id = :id";
+
+		$statement = $connection->prepare($sql);
+		$statement->bindParam(':fullname', $_POST['fullname'], PDO::PARAM_STR);
+		$statement->bindParam(':mykad', $_POST['mykad'], PDO::PARAM_STR);
+		$statement->bindParam(':email', $_POST['email'], PDO::PARAM_STR);
+		$statement->bindParam(':date_registered', $_POST['date_registered'], PDO::PARAM_STR);
+		$statement->bindParam(':id', $_POST['id'], PDO::PARAM_STR);
+	    $statement->execute();
+	} catch(PDOException $error) {
+		echo $sql . "<br>" . $error->getMessage();
+	} 
 }
 
 function deleteMember() {
