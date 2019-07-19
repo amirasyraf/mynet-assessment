7c9ab598b5b1a1484a26c4f3b55ac1e766bea041
amirasyraf <amirasyraf@outlook.com>
2019-07-19 15:04:49 +0000

    Database config

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "localhost";
+$username = "amir";
+$password = "mpc123!@#";
+$dbname = "mynet";
+$dsn        = "mysql:host=$host;dbname=$dbname";
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
+
+try {
+	$connection = new PDO($dsn, $username, $password, $options);
+} 
+
+catch(PDOException $error) {
+
+	echo $sql . "<br>" . $error->getMessage();
+}
