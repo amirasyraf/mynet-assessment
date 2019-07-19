3c519deac7ecec7c22a7d025dab279a793363842
amirasyraf <amirasyraf@outlook.com>
2019-07-19 16:46:29 +0000

    refactor

--- crud.php
+++ crud.php
@@ -53,23 +53,42 @@ function updateMember() {
 	    WHERE id = :id";
 
 		$statement = $connection->prepare($sql);
-		$statement->bindParam(':fullname', $_POST['fullname'], PDO::PARAM_STR);
-		$statement->bindParam(':mykad', $_POST['mykad'], PDO::PARAM_STR);
-		$statement->bindParam(':email', $_POST['email'], PDO::PARAM_STR);
-		$statement->bindParam(':date_registered', $_POST['date_registered'], PDO::PARAM_STR);
-		$statement->bindParam(':id', $_POST['id'], PDO::PARAM_STR);
+		$statement->bindParam(':fullname', $_POST['update-fullname'], PDO::PARAM_STR);
+		$statement->bindParam(':mykad', $_POST['update-mykad'], PDO::PARAM_STR);
+		$statement->bindParam(':email', $_POST['update-email'], PDO::PARAM_STR);
+		$statement->bindParam(':date_registered', $_POST['update-date_registered'], PDO::PARAM_STR);
+		$statement->bindParam(':id', $_POST['update-id'], PDO::PARAM_STR);
 	    $statement->execute();
 	} catch(PDOException $error) {
 		echo $sql . "<br>" . $error->getMessage();
-	} 
+	}
 }
 
 function deleteMember() {
+	global $connection;
+
+	if (isset($_POST['delete'])) { 
+		$id = $_POST['id'];
+		echo $id;
+		$sql = sprintf("DELETE FROM member WHERE id = (%s)", $id);
+
+		try {
+			$statement = $connection->prepare($sql);
+		    $statement->execute();
+		    echo "<meta http-equiv='refresh' content='0'>";
+		}
+
+		catch(PDOException $error) {
+			echo $sql . "<br>" . $error->getMessage();
+		}
+	}
 }
 
 if (isset($_POST['update'])) {
+	updateMember();
 }
 if (isset($_POST['delete'])) {
+	deleteMember();
 }
 if (isset($_POST['add'])) {
 	addMember();
